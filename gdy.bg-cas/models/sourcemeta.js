f4e06853f378bbfa1415c8f58ad1c84ea8aefa22
elvin <ali.elvin@gmail.com>
2016-11-21 21:33:51 +0000

    replace dynamoose with vogels

--- gdy.bg-cas/models/sourcemeta.js
+++ gdy.bg-cas/models/sourcemeta.js
@@ -1,50 +1,61 @@
-// var mongoose = require('mongoose');
+// var dynamoose = require('dynamoose');
 
-// var sourceMetaSchema = mongoose.Schema({
-
-// 	name: {
-// 		type: String
-// 	},
-// 	favicon: {
-// 		type: String
-// 	},
-// 	stamp: {
-// 		type: Date,
-// 		default: Date.now
-// 	},
-// 	filterBy: {
-// 		type: Boolean,
-// 		default: false
-// 	}
-
+// dynamoose.AWS.config.update({
+//   accessKeyId: 'AKID',
+//   secretAccessKey: 'SECRET',
+//   region: 'eu-west-1'
 // });
-// var uuid = require('uuid');
-var dynamoose = require('dynamoose');
 
-dynamoose.AWS.config.update({
+// dynamoose.local();
+var Joi = require('joi');
+var vogels = require('vogels');
+// vogels.AWS.config.update({accessKeyId: 'AKID', secretAccessKey: 'SECRET', region: "eu-west-1"});
+// vogels.local();
+
+var AWS = require('aws-sdk');
+
+// var opts = { endpoint : 'http://localhost:8000', apiVersion: '2012-08-10' };
+vogels.dynamoDriver(new AWS.DynamoDB());
+
+vogels.AWS.config.update({
   accessKeyId: 'AKID',
   secretAccessKey: 'SECRET',
-  region: 'eu-west-1'
+  region: 'eu-west-1',
+  endpoint : 'http://localhost:8000'
 });
 
-dynamoose.local();
 
-module.exports = dynamoose.model(
-    'SourceMeta', {
-        name: {
-            type: String,
-            hashKey: true
-        },
-        favicon: {
-            type: String
-        },
-        stamp: {
-            type: Date,
-            default: Date.now
-        },
-        filterBy: {
-            type: Boolean,
-            default: false
-        }
+module.exports = vogels.define('SourceMeta', {
+  hashKey : 'name',
+ 
+  // add the timestamp attributes (updatedAt, createdAt) 
+  timestamps : true,
+ 
+  schema : {
+    name        : Joi.string(),
+    favicon     : Joi.string(),
+    filterBy    : Joi.boolean().default(false)
+  }
+});
 
-    });
+
+
+// dynamoose.model(
+//     'SourceMeta', {
+//         name: {
+//             type: String,
+//             hashKey: true
+//         },
+//         favicon: {
+//             type: String
+//         },
+//         stamp: {
+//             type: Date,
+//             default: Date.now
+//         },
+//         filterBy: {
+//             type: Boolean,
+//             default: false
+//         }
+
+//     });
--- gdy.bg-cas/server.js
+++ gdy.bg-cas/server.js
@@ -3,25 +3,45 @@ var helper = require('./sources/helper');
 
 var SourceMeta = require('./models/sourcemeta');
 
-var dynamoose = require('dynamoose');
 var uuid = require('uuid');
 
-dynamoose.AWS.config.update({
+// var dynamoose = require('dynamoose');
+// dynamoose.AWS.config.update({
+//   accessKeyId: 'AKID',
+//   secretAccessKey: 'SECRET',
+//   region: 'eu-west-1'
+// });
+// dynamoose.local();
+
+var vogels = require('vogels');
+// vogels.AWS.config.update({accessKeyId: 'AKID', secretAccessKey: 'SECRET', region: "eu-west-1"});
+// vogels.local();
+
+var AWS = require('aws-sdk');
+// var opts = { endpoint : 'http://localhost:8000', apiVersion: '2012-08-10' };
+// vogels.dynamoDriver(new AWS.DynamoDB(opts));
+
+vogels.dynamoDriver(new AWS.DynamoDB());
+
+vogels.AWS.config.update({
   accessKeyId: 'AKID',
   secretAccessKey: 'SECRET',
-  region: 'eu-west-1'
+  region: 'eu-west-1',
+  endpoint : 'http://localhost:8000'
 });
 
-dynamoose.local();
+var stylist = new SourceMeta({name: 'stylist'});
+
+stylist.save(function(err, metas) {
+    console.log('created SourceMeta in DynamoDB', stylist.get('name'));
+
+    console.dir(metas);
+
+    // SourceMeta.scan().exec(callback);
+});
 
 
-// var uid = uuid.v4();
-
-// var stylist = new SourceMeta({name: 'stylist'});
-
-// stylist.save();
-
-SourceMeta.scan({}, function (err, metas) {
+var callback = function (err, metas) {
   
   // console.dir(metas);
 
@@ -34,40 +54,42 @@ SourceMeta.scan({}, function (err, metas) {
 
   	console.log(doc.name);
 
-	var tasks = [];
-	var source = new require('./sources/' + doc.name);
-	tasks.push(function(done) {
-        console.log('push source.meta');
-        source.meta(done, doc.name + ' meta done');
-    });
+	// var tasks = [];
+	// var source = new require('./sources/' + doc.name);
+	// tasks.push(function(done) {
+ //        console.log('push source.meta');
+ //        source.meta(done, doc.name + ' meta done');
+ //    });
 	
-	tasks.push(function(arg1, done) {
-		console.log('push source.xray');
-		source.xray(done, doc.name + ' xray done');
-	});
-		tasks.push(function(arg1, arg2, done) {
-			// console.log('3 : ' + arg2);
-			// persist and validate and send email to admin
-			helper.persistCompetitions(arg1, function(err, msg, validation) {
-				// if (validation.length > 0) {
-				// 	helper.persistValidation(validation, function() {
-				// 		util.sendAdminEmail(validation[0].source, validation);
-				// 		done(null, '4 : ' + arg1[0].source + ' validation and competitions persisted');
-				// 	});
-				// } else {
-					done(null, '4 : ' + arg1[0].source + ' competitions persisted');
-				// }
-			});
-		});
+	// tasks.push(function(arg1, done) {
+	// 	console.log('push source.xray');
+	// 	source.xray(done, doc.name + ' xray done');
+	// });
+	// 	tasks.push(function(arg1, arg2, done) {
+	// 		// console.log('3 : ' + arg2);
+	// 		// persist and validate and send email to admin
+	// 		helper.persistCompetitions(arg1, function(err, msg, validation) {
+	// 			// if (validation.length > 0) {
+	// 			// 	helper.persistValidation(validation, function() {
+	// 			// 		util.sendAdminEmail(validation[0].source, validation);
+	// 			// 		done(null, '4 : ' + arg1[0].source + ' validation and competitions persisted');
+	// 			// 	});
+	// 			// } else {
+	// 				done(null, '4 : ' + arg1[0].source + ' competitions persisted');
+	// 			// }
+	// 		});
+	// 	});
 
-	console.dir(tasks);
+	// console.dir(tasks);
 
-	async.waterfall(tasks, function(err, results) {
-		if (err) console.log(error);
-		console.log('>>> DONE ', results);
-	});
+	// async.waterfall(tasks, function(err, results) {
+	// 	if (err) console.log(error);
+	// 	console.log('>>> DONE ', results);
+	// });
 
  });		
 
-});
+};
+
+
 
