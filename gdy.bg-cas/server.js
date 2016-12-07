ebe2fef23f110324c6368d2a4f0d8e024c620827
Elvin Ali <elvin.ali@monitisecreate.com>
2016-12-07 11:46:29 +0000

    add local creds

--- gdy.bg-cas/server.js
+++ gdy.bg-cas/server.js
@@ -14,62 +14,91 @@ var async = require("async");
 var helper = require('./sources/helper');
 var sourcemeta = require('./models/sourcemeta');
 
+if (process.env.NODE_ENV ==='local') 
+  AWS.config.loadFromPath(__dirname + '/credentials_local.json');
+else 
+  AWS.config.loadFromPath(__dirname + '/credentials.json');
 
-AWS.config.loadFromPath(__dirname + '/credentials.json');
 logger.info(AWS.config);
+// tables will be created only if they do not exist
 
+sourcemeta.describeTable(function(err, tableInfo) {
 
-// insert all sources into sourcemetas
-var sources = JSON.parse(require('fs').readFileSync(__dirname +
-    '/sources/sources.json', 'utf8'));
+    // I do not want this error logged
+    // if (err) logger.error(err);
 
-sourcemeta.update(sources, function(err, result) {
-    if (err) logger.error(err);
+    // if table sourcemetas exist, drop it before recreating
+    // that is because I can add additional sources to sources.json
 
-    console.dir(result);
-    console.dir(sources);
-
-    sources.forEach(function(doc) {
-
-        var tasks = [];
-        var source = new require('./sources/' + doc.name);
-
-        async.waterfall([
-            function(callback) {
-                // callback(null, 'one', 'two');
-                logger.info('ADD TASK ' + doc.name +
-                    '.meta');
-                source.meta(callback(null, doc.name +
-                    '.meta'
-                ));
-            },
-            function(arg1, callback) {
-                logger.info('ADD TASK ' + doc.name +
-                    '.xray');
-                // arg1 now equals 'one' and arg2 now equals 'two'
-                source.xray(function(err, result) {
-                    // logger.info('end');
-                    callback(null, result, arg1 + ',' + doc.name +
-                        '.xray');
-                });
-            },
-            function(arg1, arg2, callback) {
-                logger.info('ADD TASK ' + doc.name +
-                    '.persistCompetitions');
-                // arg1 now equals 'one' and arg2 now equals 'two'
-                helper.persistCompetitions(arg1, function(err,
-                    msg) {
-                    // logger.info('done');
-                    callback(null, arg2 + ',' + doc.name +
-                        '.persistCompetitions'
-                    );
-                });
-            }
-
-        ], function(err, result) {
+    if (typeof tableInfo !== 'undefined') {
+        sourcemeta.deleteTable(function(err) {
             if (err) logger.error(err);
-            logger.info('TASKS COMPLETED', result);
+            seed();
+        });
+    } else {
+        seed();
+    }
+
+});
+
+
+var seed = function() {
+
+    vogels.createTables(function(err) {
+        if (err) logger.error(err);
+
+        // insert all sources into sourcemetas
+        var sources = JSON.parse(require('fs').readFileSync(__dirname +
+            '/sources/sources.json', 'utf8'));
+
+        sourcemeta.create(sources, function(err, result) {
+            if (err) logger.error(err);
+
+            result.forEach(function(doc) {
+
+                var tasks = [];
+                var source = new require('./sources/' + doc.attrs.name);
+
+                async.waterfall([
+                    function(callback) {
+                        // callback(null, 'one', 'two');
+                        logger.info('ADD TASK ' + doc.attrs.name +
+                            '.meta');
+                        source.meta(callback(null, doc.attrs.name +
+                            '.meta'
+                        ));
+                    },
+                    function(arg1, callback) {
+                        logger.info('ADD TASK ' + doc.attrs.name +
+                            '.xray');
+                        // arg1 now equals 'one' and arg2 now equals 'two'
+                        source.xray(function(err, result) {
+                            // logger.info('end');
+                            callback(null, result, arg1 + ',' + doc.attrs
+                                .name +
+                                '.xray');
+                        });
+                    },
+                    function(arg1, arg2, callback) {
+                        logger.info('ADD TASK ' + doc.attrs.name +
+                            '.persistCompetitions');
+                        // arg1 now equals 'one' and arg2 now equals 'two'
+                        helper.persistCompetitions(arg1, function(err,
+                            msg) {
+                            // logger.info('done');
+                            callback(null, arg2 + ',' + doc.attrs.name +
+                                '.persistCompetitions'
+                            );
+                        });
+                    }
+
+                ], function(err, result) {
+                    if (err) logger.error(err);
+                    logger.info('TASKS COMPLETED', result);
+                });
+
+            });
         });
 
     });
-});
+};
