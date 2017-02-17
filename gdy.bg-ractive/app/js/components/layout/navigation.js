952beb5c9a9af9f6362701e3159631ef9b32303d
Elvin Ali <elvin.ali@monitisecreate.com>
2017-02-17 17:18:41 +0000

    add search term observer

--- gdy.bg-ractive/app/js/components/layout/navigation.js
+++ gdy.bg-ractive/app/js/components/layout/navigation.js
@@ -10,10 +10,8 @@ var Navigation = Ractive.components.Navigation = Ractive.extend({
 
         this.observe('req', (request) => {
             if (request.body.navigation && request.body.navigation.sources) {
-                console.log('SET SOURCES ' + request.body.navigation.sources);
                 this.set('sources', request.body.navigation.sources);
             }
-
         });
     }
 });
