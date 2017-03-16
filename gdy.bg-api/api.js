998788d3f006f0534b2f32b68727c3dec3254bf4
Elvin Ali <elvin.ali@monitisecreate.com>
2017-03-16 21:55:56 +0000

    rename sourcemetas to sources

--- gdy.bg-api/api.js
+++ gdy.bg-api/api.js
@@ -16,7 +16,7 @@ exports.getCompetitions = function (event, cb) {
 exports.getSources = function (event, cb) {
     console.log("getSources", JSON.stringify(event));
 
-    Sourcemeta.scan().exec(function (err, data) {
+    Source.scan().exec(function (err, data) {
         if (err) cb(err);
         var result = {
             body: data
