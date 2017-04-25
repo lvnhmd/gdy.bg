7d90133b4695e1c1be6eda1bb2a5e1a795837203
Elvin Ali <elvin.ali@monitisecreate.com>
2017-04-25 18:42:15 +0100

    move fb login func to ractive

--- gdy.bg-ractive/app/js/components/layout/login.js
+++ gdy.bg-ractive/app/js/components/layout/login.js
@@ -3,13 +3,107 @@ import Template from '../../../views/login.html';
 import * as router from '../../plugins/router';
 
 var Login = Ractive.components.Login = Ractive.extend({
-    template: Template,
-    
-    oninit() {
+	template: Template,
+
+	oninit() {
 		this.on('login', (rEvent) => {
 			rEvent.original.preventDefault();
 			router.navTo(`/login`);
 		});
+
+		this.on('testAPI', (rEvent) => {
+			alert('HELO HOLA');
+			// rEvent.original.preventDefault();
+			// router.navTo(`/`);
+		});
+
+	},
+
+	oncomplete() {
+
+		this.on('checkLoginState', function () {
+			FB.getLoginStatus(function (response) {
+				statusChangeCallback(response);
+			});
+		});
+
+
+		// This is called with the results from from FB.getLoginStatus().
+		function statusChangeCallback(response) {
+			alert('statusChangeCallback');
+			console.log('statusChangeCallback');
+			console.log(response);
+			// The response object is returned with a status field that lets the
+			// app know the current login status of the person.
+			// Full docs on the response object can be found in the documentation
+			// for FB.getLoginStatus().
+			if (response.status === 'connected') {
+				// Logged into your app and Facebook.
+				testAPI();
+			} else {
+				// The person is not logged into your app or we are unable to tell.
+				document.getElementById('status').innerHTML = 'Please log ' +
+					'into this app.';
+			}
+		}
+
+		// This function is called when someone finishes with the Login
+		// Button.  See the onlogin handler attached to it in the sample
+		// code below.
+		function checkLoginState() {
+			FB.getLoginStatus(function (response) {
+				statusChangeCallback(response);
+			});
+		}
+
+		window.fbAsyncInit = function () {
+			alert('window.fbAsyncInit');
+			FB.init({
+				appId: '809916329143027',
+				cookie: false,  // enable cookies to allow the server to access 
+				// the session
+				xfbml: true,  // parse social plugins on this page
+				version: 'v2.8' // use graph api version 2.8
+			});
+
+			// Now that we've initialized the JavaScript SDK, we call 
+			// FB.getLoginStatus().  This function gets the state of the
+			// person visiting this page and can return one of three states to
+			// the callback you provide.  They can be:
+			//
+			// 1. Logged into your app ('connected')
+			// 2. Logged into Facebook, but not your app ('not_authorized')
+			// 3. Not logged into Facebook and can't tell if they are logged into
+			//    your app or not.
+			//
+			// These three cases are handled in the callback function.
+
+			FB.getLoginStatus(function (response) {
+				alert('getLoginStatus');
+				statusChangeCallback(response);
+			});
+
+		};
+		// Here we run a very simple test of the Graph API after login is
+		// successful.  See statusChangeCallback() for when this call is made.
+		function testAPI() {
+			console.log('Welcome!  Fetching your information.... ');
+			FB.api('/me', function (response) {
+				console.log('Successful login for: ' + JSON.stringify(response));
+				//here I want to navigate back to index.html and display name in the header  
+				document.getElementById('status').innerHTML =
+					'Thanks for logging in, ' + response.name + '!';
+				//go back to homepage 
+				//set user in response     
+				//can I invoke a ractive function ? 
+				$('#testAPI').trigger('click');
+				//    alert($('#testAPI'));
+				alert(document.getElementById('testAPI'));
+				alert('HMM');
+
+			});
+		}
+
 	}
 
 });
