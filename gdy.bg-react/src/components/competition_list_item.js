e4db841d3bdd87b61445ff82116f7ddd290b3d3d
Elvin Ali <elvin.ali@monitisecreate.com>
2017-07-01 20:05:54 +0100

    add link to open competition in a new tab

--- gdy.bg-react/src/components/competition_list_item.js
+++ gdy.bg-react/src/components/competition_list_item.js
@@ -14,7 +14,7 @@ export default class CompetitionListItem extends React.Component {
             <div className="product-index desktop-4 tablet-2 mobile-3">
                 <div className="product-index-inner">
                     <div className="badge">{competition.daystoenter}</div>
-                    <a onClick={this.click.bind(this)}><div className="img-responsive img-thumbnail ratio-4-3" style={{ backgroundImage: "url('" + competition.img + "')" }} /></a>
+                    <a onClick={this.click.bind(this)} href={competition.uri} target="_blank"><div className="img-responsive img-thumbnail ratio-4-3" style={{ backgroundImage: "url('" + competition.img + "')" }} /></a>
                     <div className="product-info">{competition.title}</div>
                     {competition.title.toLowerCase().length < 52 ? <div className="blankrow">&nbsp;</div> : ''}
                     <SocialShare shareUrl="https://swagbag.club" title={competition.title} img={competition.img} hashtags={competition.hashtags} />
