6fc32c43447aca5020862fb6e2b3bfd1e7f4f866
Elvin Ali <elvin.ali@monitisecreate.com>
2017-07-03 19:48:36 +0100

    remove app.js

--- gdy.bg-react/src/index.js
+++ gdy.bg-react/src/index.js
@@ -3,14 +3,25 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import Header from './components/header';
+import SourceList from './containers/source_list';
+import CompetitionList from './containers/competition_list';
+import Login from './components/login';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Header />
+        <SourceList />
+        <Route path="/" component={CompetitionList} />
+        <Route path="/login" component={Login} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
