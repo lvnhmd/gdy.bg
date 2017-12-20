096f8bead123443670b67a8a84d40e8c13f2045a
elvin <ali.elvin@gmail.com>
2017-12-20 12:28:29 +0000

    add mobile sign in WIP

--- gdy.bg-static/src/components/app.js
+++ gdy.bg-static/src/components/app.js
@@ -24,10 +24,10 @@ export default class App extends React.Component {
                     html: {
                         fontSize: 16
                     },
-                    // a: {
-                    //     color: '#C00000',
-                    //     textDecoration: 'none'
-                    // },
+                    a: {
+                        color: '#C00000',
+                        textDecoration: 'none'
+                    },
                     'a:hover': {
                         textDecoration: 'underline'
                     },
@@ -171,6 +171,52 @@ export default class App extends React.Component {
                             },
                             '.n-main__nav-search': {
                                 borderLeft: '1px solid #EAEAEA'
+                            },
+                            h1: {
+                                fontSize: '3.125rem'
+                            },
+                            '.nl-main__wrapper': {
+                                paddingLeft: 60,
+                                paddingRight: 60
+                            },
+                            '.nl-main__title': {
+                                fontSize: '3.125rem',
+                                lineHeight: '3.125rem',
+                                margin: '60px 0 20px 0'
+                            },
+                            '.nl-main__content': {
+                                fontSize: '1.125rem',
+                                lineHeight: '1.5625rem',
+                                margin: '20px 0 50px 0'
+                            },
+                            '.nl-form__checkbox-title': {
+                                fontSize: '1.8125rem',
+                                lineHeight: '2rem'
+                            },
+                            '.nl-form__checkbox-content': {
+                                marginBottom: 30
+                            },
+                            '.nl-form__email': {
+                                display: 'inline-block',
+                                marginBottom: 30,
+                                marginRight: 10,
+                                marginTop: 0,
+                                width: 420
+                            },
+                            '.nl-form__email-label': {
+                                marginTop: 60
+                            },
+                            '.nl-form__submit': {
+                                display: 'inline-block',
+                                marginBottom: 30,
+                                marginTop: 0,
+                                width: 120
+                            },
+                            '.nl-main__separator': {
+                                marginTop: 40
+                            },
+                            '.nl-main__footer': {
+                                marginBottom: 60
                             }
                         },
                         'screen and (min-width: 92.5em)': {
@@ -185,6 +231,10 @@ export default class App extends React.Component {
                             '.srch-textbox': {
                                 fontSize: '3.125rem',
                                 lineHeight: '3.125rem'
+                            },
+                            '.nl-main__wrapper': {
+                                paddingLeft: 60,
+                                paddingRight: 60
                             }
                         },
                         'screen and (max-width: 64em)': {
@@ -429,7 +479,7 @@ export default class App extends React.Component {
                     '.n-main__dropdown-list--right': {
                         right: -1
                     },
-                    //media
+
                     '.n-main__nav-search': {
                         display: 'inline-block',
                         float: 'right',
@@ -748,7 +798,179 @@ export default class App extends React.Component {
                     h3: {
                         fontSize: '1.25rem',
                         fontWeight: 400
+                    },
+                    // login page style
+
+                    'h1, h3, h4': {
+                        color: '#111',
+                        fontFamily: '"ITC Caslon", Georgia, serif',
+                        fontWeight: 400,
+                        lineHeight: '1.2em',
+                        letterSpacing: '-0.01em',
+                        WebkitFontVariantLigatures: 'common-ligatures',
+                        fontVariantLigatures: 'common-ligatures',
+                        margin: '30px 0 20px 0'
+                    },
+                    h1: {
+                        fontSize: '2.1875rem',
+                        fontWeight: 400
+                    },
+                    '.global__button-reset': {
+                        WebkitAppearance: 'none',
+                        msAppearance: 'none',
+                        MozAppearance: 'none',
+                        OAppearance: 'none',
+                        appearance: 'none',
+                        background: 'transparent',
+                        border: 'none',
+                        borderRadius: 0,
+                        cursor: 'pointer',
+                        padding: 0
+                    },
+                    '.nl-main__wrapper': {
+                        margin: '0 auto',
+                        maxWidth: 560,
+                        paddingLeft: 20,
+                        paddingRight: 20
+                    },
+                    '.nl-main__title': {
+                        fontFamily: '"ITC Caslon", Georgia, serif',
+                        fontSize: '2.1875rem',
+                        lineHeight: '2.25rem',
+                        letterSpacing: '-0.01em',
+                        margin: '40px 0 15px 0',
+                        textAlign: 'center'
+                    },
+                    '.nl-main__content': {
+                        color: '#666',
+                        fontFamily: '"adobe-caslon-pro", serif',
+                        fontSize: '1rem',
+                        lineHeight: '1.25rem',
+                        letterSpacing: 'normal',
+                        margin: '15px 0 30px 0',
+                        textAlign: 'center'
+                    },
+                    '.nl-form__option': {
+                        borderBottom: '1px solid #EAEAEA'
+                    },
+                    '.nl-form__checkbox': {
+                        display: 'none'
+                    },
+                    '.nl-form__checkbox:checked+.nl-form__checkbox-box': {
+                        backgroundPositionX: -20
+                    },
+                    '.nl-form__label': {
+                        cursor: 'pointer',
+                        display: 'block'
+                    },
+                    '.nl-form__checkbox-box': {
+                        backgroundImage: 'url("https://www.vogue.co.uk/static/img/newsletter_checkbox.png")',
+                        backgroundSize: '40px 20px',
+                        border: '1px solid #E0E0E0',
+                        display: 'block',
+                        float: 'left',
+                        height: 20,
+                        width: 20
+                    },
+                    '.nl-form__checkbox-title': {
+                        fontFamily: '"ITC Caslon", Georgia, serif',
+                        fontSize: '1.5rem',
+                        lineHeight: '1.75rem',
+                        letterSpacing: '-0.01em',
+                        marginBottom: 10,
+                        marginLeft: 50
+                    },
+                    '.nl-form__checkbox-content': {
+                        fontFamily: '"adobe-caslon-pro", serif',
+                        fontSize: '1rem',
+                        lineHeight: '1.5rem',
+                        letterSpacing: 'normal',
+                        marginBottom: 25,
+                        marginLeft: 50
+                    },
+                    '.nl-form__email': {
+                        backgroundColor: '#F9F9F9',
+                        border: '1px solid #E0E0E0',
+                        WebkitBoxSizing: 'border-box',
+                        boxSizing: 'border-box',
+                        display: 'block',
+                        fontFamily: 'Nobel, sans-serif',
+                        fontSize: '.875rem',
+                        lineHeight: '.9375rem',
+                        letterSpacing: '0.025em',
+                        height: 45,
+                        margin: '25px 0 10px 0',
+                        padding: 15,
+                        width: '100%'
+                    },
+                    '.nl-form__email-label': {
+                        display: 'block',
+                        fontFamily: 'Nobel, sans-serif',
+                        fontSize: '.9375rem',
+                        fontWeight: 700,
+                        lineHeight: '.9375rem',
+                        letterSpacing: '0.025em',
+                        marginTop: 40,
+                        marginBottom: 25,
+                        textTransform: 'uppercase'
+                    },
+                    '.nl-form__submit': {
+                        display: 'block',
+                        backgroundColor: '#C00000',
+                        border: '1px solid transparent',
+                        color: '#fff',
+                        textDecoration: 'none',
+                        marginTop: 10,
+                        marginBottom: 30,
+                        textTransform: 'uppercase',
+                        width: '100%',
+                        height: 45
+                    },
+                    '.nl-form__checkbox-small': {
+                        display: 'none'
+                    },
+                    '.nl-form__checkbox-small:checked+.nl-form__checkbox-small-box': {
+                        backgroundPositionX: -16
+                    },
+                    '.nl-form__checkbox-small-box': {
+                        backgroundImage: 'url("https://www.vogue.co.uk/static/img/newsletter_checkbox.png")',
+                        backgroundSize: '32px 16px',
+                        border: '1px solid #E0E0E0',
+                        display: 'block',
+                        float: 'left',
+                        height: 16,
+                        width: 16
+                    },
+                    '.nl-form__checkbox-small-content': {
+                        color: '#666',
+                        fontFamily: '"adobe-caslon-pro", serif',
+                        fontSize: '.75rem',
+                        lineHeight: '1.125rem',
+                        letterSpacing: 'normal',
+                        margin: 0,
+                        marginBottom: 20,
+                        marginLeft: 31
+                    },
+                    '.nl-main__separator': {
+                        backgroundColor: '#EAEAEA',
+                        height: 1,
+                        marginTop: 10,
+                        marginBottom: 20,
+                        width: '100%'
+                    },
+                    '.nl-main__footer': {
+                        color: '#BDBDBD',
+                        fontFamily: 'Nobel, sans-serif',
+                        fontSize: '.6875rem',
+                        lineHeight: '1.125rem',
+                        letterSpacing: '0.025em',
+                        marginBottom: 40
+                    },
+                    '.nl-main__privacy': {
+                        color: 'inherit',
+                        fontWeight: 700
                     }
+
 
                 }}
                 />
--- gdy.bg-static/src/components/signin.js
+++ gdy.bg-static/src/components/signin.js
@@ -15,31 +15,82 @@ class Signin extends Component {
 
     render() {
         return (
-            <div id="content" className="row">
-                <div id="customer-wrapper" className="desktop-12 mobile-3 has-background">
+            // <div id="content" className="row">
+            //     <div id="customer-wrapper" className="desktop-12 mobile-3 has-background">
 
-                    <div id="customer-background">
-                        <div id="template" className="desktop-5 mobile-3">
-                            <div className="clear"></div>
-                            <div id="customer" className="desktop-12 mobile-3">
-                                <SocialLogin provider='facebook'
-                                    appId={process.env.REACT_APP_FB_APPID}
-                                    callback={this.responseSocialLogin.bind(this)}>
-                                    <button>Sign-in with FB</button>
-                                </SocialLogin>
-                            </div>
-                            <div className="clear"></div>
-                            <div id="customer" className="desktop-12 mobile-3">
-                                <SocialLogin provider='google'
-                                    appId={process.env.REACT_APP_GPLUS_APPID}
-                                    callback={this.responseSocialLogin.bind(this)}>
-                                    <button>Sign-in with Google</button>
-                                </SocialLogin>
-                            </div>
+            //         <div id="customer-background">
+            //             <div id="template" className="desktop-5 mobile-3">
+            //                 <div className="clear"></div>
+            //                 <div id="customer" className="desktop-12 mobile-3">
+            //                     <SocialLogin provider='facebook'
+            //                         appId={process.env.REACT_APP_FB_APPID}
+            //                         callback={this.responseSocialLogin.bind(this)}>
+            //                         <button>Sign-in with FB</button>
+            //                     </SocialLogin>
+            //                 </div>
+            //                 <div className="clear"></div>
+            //                 <div id="customer" className="desktop-12 mobile-3">
+            //                     <SocialLogin provider='google'
+            //                         appId={process.env.REACT_APP_GPLUS_APPID}
+            //                         callback={this.responseSocialLogin.bind(this)}>
+            //                         <button>Sign-in with Google</button>
+            //                     </SocialLogin>
+            //                 </div>
+            //             </div>
+            //         </div>
+            //     </div>
+            // </div>
+            <main className="nl-main">
+                <div className="nl-main__wrapper">
+                    <h1 className='nl-main__title'></h1>
+                    <p className='nl-main__content'></p>
+
+                    <form className="nl-form" method="post" action="">
+                        <div className="nl-form__option">
+                            <label className="nl-form__label" htmlFor="nl-form__checkbox-2">
+                                <input name="chk_daily" value="y" type="checkbox" id="nl-form__checkbox-2"
+                                    className='nl-form__checkbox' />
+                                <span className="nl-form__checkbox-box"></span>
+                                <h3 className='nl-form__checkbox-title'>Vogue Daily</h3>
+                                <p className='nl-form__checkbox-content'>Your daily Vogue digest - the day's biggest fashion news, beauty tips, shopping and trends inspiration,
+                                red-carpet galleries and more</p>
+                            </label>
                         </div>
+                        <div className="nl-form__option">
+                            <label className="nl-form__label" htmlFor="nl-form__checkbox-3">
+                                <input name="chk_weekly" value="y" type="checkbox" id="nl-form__checkbox-3" className='nl-form__checkbox' />
+                                <span className="nl-form__checkbox-box"></span>
+                                <h3 className='nl-form__checkbox-title'>Vogue Weekly</h3>
+                                <p className='nl-form__checkbox-content'>Catch up with this week in Vogue with our weekend newsletters</p>
+                            </label>
+                        </div>
+
+                        <label className='nl-form__email-label' htmlFor="nl-form__email">Enter your e-mail address</label>
+                        <input type="email" name="email" className='nl-form__email' id="nl-form__email" placeholder="Enter your e-mail address" required
+                            value="" />
+
+                        <input type="submit" name="" className='global__button-reset btn-single btn-single--light nl-form__submit' value='Sign Up' />
+
+
+                        <label htmlFor='nl-form__checkbox-5' className='nl-form__label'>
+                            <input type="checkbox" name="optin_condenast" value="y" id='nl-form__checkbox-5' className='nl-form__checkbox-small' />
+                            <span className="nl-form__checkbox-small-box"></span>
+                            <p className='nl-form__checkbox-small-content'>I would like to receive news and special offers from Cond&eacute; Nast Publications</p>
+                        </label>
+
+                        <label htmlFor='nl-form__checkbox-6' className='nl-form__label'>
+                            <input type="checkbox" name="optin_thirdparty" value="y" id='nl-form__checkbox-6' className='nl-form__checkbox-small' />
+                            <span className="nl-form__checkbox-small-box"></span>
+                            <p className='nl-form__checkbox-small-content'>I would like to receive news and special offers from carefully selected partners of Cond&eacute; Nast</p>
+                        </label>
+                    </form>
+
+                    <div className='nl-main__separator'></div>
+                    <div className='nl-main__footer'>Will be used in accordance with our&nbsp;
+                    <a href='http://www.condenast.co.uk/privacy' className='nl-main__privacy'>Privacy Policy</a>
                     </div>
                 </div>
-            </div>
+            </main>
         );
     }
 }
