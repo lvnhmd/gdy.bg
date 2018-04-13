053c99c367172d555ac1bc9fce2a6c7367436998
Elvin Ali <elvin.ali@monitisecreate.com>
2018-04-13 18:22:40 +0100

    Configure more crawlers

--- gdy.bg-cas-v2/crawl.js
+++ gdy.bg-cas-v2/crawl.js
@@ -1,71 +1,157 @@
-import { isUndefined } from "util";
-
 var Crawler = require("simplecrawler");
 var cheerio = require("cheerio");
 
-var config = JSON.parse(require('fs')
-    .readFileSync(__dirname + '/source-config.json', 'utf8'));
-
-// console.log(config['stylist'].title);    
-// data[i].url = data[i].url;
-//                 data[i].img = data[i].img;
-//                 data[i].source = 'stylist';
-//                 data[i].title = _.trim(data[i].title).replace(/\r?\n|\r/g, '').substr(0, 95);
-//                 ClosingDate
-// /json">(.+)<\/script>/U
-var crawler = Crawler("https://www.stylist.co.uk/win")
+var stylistCrawler = Crawler("https://www.stylist.co.uk/win/")
     .on("fetchcomplete", function (queueItem, responseBuffer, response) {
 
-        if (queueItem.url.match(/\d$/i)) {
-            console.log('-----------------------------------------------------------------------');
-            console.log(queueItem.url);
-            let responseStr = responseBuffer.toString("utf8");
-            let $ = cheerio.load(responseStr);
-            
-            console.log($(config['stylist'].title).text());
-            let img = $(config['stylist'].img).attr("src")
-            console.log(img);
-            if(isUndefined(img)) {
-                // extract 
-            }
-            // check the page for a form
-            // console.log($(config['stylist'].entryForm).text());
-            // this means that we have an entry form therefore is a viable competition 
-            if(responseStr.indexOf(config['stylist']['data-widget'])>-1){
-                // look for the date with a pattern
-                
-                let match = new RegExp(config['stylist'].extractDateRegex).exec(responseStr);
-                if(null != match)
-                    console.log(match[0]);
-                else console.log(match);
+        if (queueItem.url.match(/.+(get-|win-).+\d\d+$/i)) {
 
+            let responseStr = responseBuffer.toString("utf8").replace(/\r?\n|\r/g, '');
+            try {
+                let json = JSON.parse(/({.*})<\/script>/.exec(/json\">(.+)<\/script>/g.exec(responseStr)[1])[1]);
+
+                let keywords = ['Win', 'Sponsored', 'Events'];
+
+                if (keywords.some(v => json.keywords.includes(v))) {
+
+                    let match = /closes.*((?:\d{1}|\d{2})\/\d{2}\/\d{2})/.exec(responseStr);
+                    let closingDate = !match ? '' : match[1];
+                    let comp = {
+                        url: json.url,
+                        img: json.image[0],
+                        title: json.headline,
+                        closingDate: closingDate,
+                        publisher: 'stylist'
+                    };
+                    console.log('-----------------------------------------------------------------------');
+                    console.log(comp);
+                    console.log('-----------------------------------------------------------------------');
+                }
+            } catch (error) {
+                console.log('NOT SCRAPING ', queueItem.url);
             }
-            // console.log(responseStr.indexOf(config['stylist']['data-widget']));
-            console.log('-----------------------------------------------------------------------');
-            // Enter now
         }
     });
 
-//     crawler.discoverResources = function (responseBuffer, queueItem) {
-//     var $ = cheerio.load(responseBuffer.toString("utf8"));
+stylistCrawler.maxDepth = 2;
 
-//     // return $("a[href]").map(function () {
-//     //     let url = $(this).attr("href");
-//     //     if(url.indexOf('win'))
-//     //         return url;
-//     // }).get();
+// stylistCrawler.start();
 
-//     return $('a[href]').filter(function (i, el) {
-//         // this === el
-//         return $(this).attr('href').indexOf('win') > 0;
-//     }).attr('href');
-// };
+var shortlistCrawler = Crawler("https://www.shortlist.com/win/")
+    .on("fetchcomplete", function (queueItem, responseBuffer, response) {
+        // console.log(queueItem.url);
+        if (queueItem.url.match(/.+(\/win|get-|win-).+\d\d+$/i)) {
 
-// crawler.addFetchCondition(function (queueItem, referrerQueueItem, callback) {
-//     // callback(null, queueItem.path.match(/win.*\d$/i));
-//     callback(null, queueItem.path.match(/\d$/i));
-// });
+            let responseStr = responseBuffer.toString("utf8").replace(/\r?\n|\r/g, '');
+            try {
+                let json = JSON.parse(/({.*})<\/script>/.exec(/json\">(.+)<\/script>/g.exec(responseStr)[1])[1]);
 
-crawler.maxDepth = 2;
+                // let keywords = ['Win', 'Sponsored', 'Events'];
 
-crawler.start();
+                // if (keywords.some(v => json.keywords.includes(v))) {
+
+                let match = /closes.*((?:\d{1}|\d{2})\/\d{2}\/\d{2})/.exec(responseStr);
+                let closingDate = !match ? '' : match[1];
+                let comp = {
+                    url: json.url,
+                    img: json.image[0],
+                    title: json.headline,
+                    closingDate: closingDate,
+                    publisher: 'shortlist'
+                };
+                console.log('-----------------------------------------------------------------------');
+                console.log(comp);
+                console.log('-----------------------------------------------------------------------');
+                // }
+            } catch (error) {
+                console.log('NOT SCRAPING ', queueItem.url);
+            }
+        }
+    });
+
+shortlistCrawler.maxDepth = 2;
+
+// shortlistCrawler.start();
+
+
+var start = new Date();
+console.log("BEGIN CRAWLING @" + start);
+
+var mrhydeCrawler = Crawler("https://www.mrhyde.com/")
+    .on("fetchcomplete", function (queueItem, responseBuffer, response) {
+
+        if (queueItem.url.match(/.+(competitions|offers).+\d\d+$/i)) {
+            console.log('-----------------------------------------------------------------------');
+            let responseStr = responseBuffer.toString("utf8").replace(/\r?\n|\r/g, '');
+            try {
+                let json = JSON.parse(/({.*})<\/script>/.exec(/json\">(.+)<\/script>/g.exec(responseStr)[1])[1]);
+
+                let match = /closes.*((?:\d{1}|\d{2})\/\d{2}\/\d{2})/.exec(responseStr);
+                let closingDate = !match ? '' : match[1];
+                let comp = {
+                    url: json.url,
+                    img: json.thumbnailUrl,
+                    title: json.headline,
+                    closingDate: closingDate,
+                    publisher: 'mrhyde'
+                };
+                console.log('-----------------------------------------------------------------------');
+                console.log(comp);
+                console.log('-----------------------------------------------------------------------');
+                // }
+            } catch (error) {
+                console.log('NOT SCRAPING ', queueItem.url);
+            }
+        }
+    });
+
+mrhydeCrawler.on('complete', function () {
+    let now = new Date();
+    console.log("END CRAWLING @" + now + ", execution time in sec :" + (now - start) / 1000 + 's');
+});
+
+mrhydeCrawler.maxDepth = 3;
+
+// mrhydeCrawler.start();
+
+
+var start = new Date();
+console.log("BEGIN CRAWLING @" + start);
+
+var cntravellerCrawler = Crawler("http://www.cntraveller.com/topic/competitions")
+    .on("fetchcomplete", function (queueItem, responseBuffer, response) {
+
+        if (queueItem.url.match(/.+(win-|competition).+/i)) {
+            console.log(queueItem.url);
+            //     console.log('-----------------------------------------------------------------------');
+            //     let responseStr = responseBuffer.toString("utf8").replace(/\r?\n|\r/g , '') ;
+            //     try {
+            //         let json = JSON.parse(/({.*})<\/script>/.exec(/json\">(.+)<\/script>/g.exec(responseStr)[1])[1]);
+
+            //         let match = /closes.*((?:\d{1}|\d{2})\/\d{2}\/\d{2})/.exec(responseStr);
+            //         let closingDate = !match ? '' : match[1];
+            //         let comp = {
+            //             url: json.url,
+            //             img: json.thumbnailUrl,
+            //             title: json.headline,
+            //             closingDate: closingDate,
+            //             publisher: 'mrhyde'
+            //         };
+            //         console.log('-----------------------------------------------------------------------');
+            //         console.log(comp);
+            //         console.log('-----------------------------------------------------------------------');
+            //         // }
+            //     } catch (error) {
+            //         console.log('NOT SCRAPING ', queueItem.url);
+            //     }
+        }
+    });
+
+cntravellerCrawler.on('complete', function () {
+    let now = new Date();
+    console.log("END CRAWLING @" + now + ", execution time in sec :" + (now - start) / 1000 + 's');
+});
+
+cntravellerCrawler.maxDepth = 3;
+
+cntravellerCrawler.start();
